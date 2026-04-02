5c367b26361cedac28f5ebb41301985086c555e5
lukagbn <luka68690@gmail.com>
2026-04-02 17:17:16 +0400

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,8 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${inter.className}`}>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import layout from "@/app/layout.module.scss";
+import Logo from "../Icons/Logo/Logo";
+import Image from "next/image";
+import styles from "./Navbar.module.scss";
+import User from "../Icons/User/User";
+
+function Navbar() {
+  const NAV_LIST = [
+    { img: "/icons/stars.svg", title: "browse courses" },
+    { img: "/icons/book.svg", title: "enrolled courses" },
+  ];
+  return (
+    <>
+      <header className={`${styles.header} ${layout.container}`}>
+        <Logo />
+        <div className={styles.navList}>
+          {NAV_LIST.map((list) => (
+            <div key={list.title} className={styles.list}>
+              <Image width={26} height={26} alt={list.title} src={list.img} />
+              <p>{list.title}</p>
+            </div>
+          ))}
+          <User />
+        </div>
+      </header>
+      <hr className={styles.hr} />
+    </>
+  );
+}
+
+export default Navbar;
